0d2dd76a65e24466627e333994eb51110298f23c
domin <domin@Andrys-PC>
2022-03-30 00:53:27 -0400

    first commit

--- src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
@@ -13,6 +13,7 @@ public class LinearSearch {
     public static int search(int arr[], int x)
     {
         int n = arr.length;
+        
         // Todo 01: - complete the implementation of linear search and test your code  
          //         - prvoide asymptotic analysis of the developed solution
     }
